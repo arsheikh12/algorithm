6704dcf903ffa268fd11ed5955c2b91e1eba4fe0
Abdur Razzak <arsduet2122@gmail.com>
2025-04-23 00:56:29 +0600

    Update lcs.cpp

--- lcs.cpp
+++ lcs.cpp
@@ -1,48 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-void LCS_algo(string x, string y, int m, int n){
-    m = x.size(),n = y.size();
-    int i, j,index;
-    int L[m+1][n+1];
-
-
-    for(int i=0; i<=m; i++){
-        L[i][0]=0;
-    }
-    for(int i=0; i<=m; i++){
-        L[0][i]=0;
-    }
-
-    for(int i=1 ;i<=m ;i++){
-        for(int j =1; j<=n; j++){
-            if(x[i-1] ==y[j-1]){
-                L[i][j] = L[i-1][j-1]+1;
-
-            }else L[i][j] = max(L[i-1][j], L[i][j-1]);
+string LCS(const string &string1, const string &string2) {
+    int n = string1.size();
+    int m = string2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= m; ++j)
+        {
+            if (string1[i - 1] == string2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+           else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
     }
-
-index = L[m][n];
-   char LCS[index + 1];
-   LCS[index] = '\0';
-   i = m, j = n;
-   while (i > 0 && j > 0) {
-      if (x[i - 1] == y[j - 1]) {
-         LCS[index - 1] = x[i - 1];
-         i--;
-         j--;
-         index--;
-      } else if (L[i - 1][j] > L[i][j - 1])
-         i--;
-      else
-         j--;
-   }
-   printf("LCS: %s\n", LCS);
+    cout<<dp[n][m]<<endl;
+    string Lsc = "";
+    int i = n, j = m;
+    while (i > 0 && j > 0) {
+        if (string1[i - 1] == string2[j - 1])
+        {
+            Lsc += string1[i - 1];
+            --i;
+            --j;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1])
+        {
+            --i;
+        }
+         else
+        {
+            --j;
+        }
+    }
+    reverse(Lsc.begin(), Lsc.end());
+    return Lsc;
 }
-int max(int a, int b){
-return(a>b)?a:b;}
-int main(){
-    string y,x= "lullabybabies";
-    y= "skullandbones";
-   LCS_algo(x,y,x.size(), y.size());
+int main() {
+    string string1 = "lullabybabies";
+    string string2 = "skullandbones";
+    string Lsc = LCS(string1, string2);
+    cout << "LCS: " << Lsc << endl;
+    return 0;
 }
